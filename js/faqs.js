84f937ce21f07c57620d4f90487ab5bcbb07c015
Dan13lpa <dan13lpa@gmail.com>
2023-03-31 13:55:06 +0200

    Last updates

--- js/faqs.js
+++ js/faqs.js
@@ -1,19 +1,11 @@
 export default (() => {
 
-    const faqsButtons = document.querySelectorAll('.faqs-button');
+    const faqsButtons = document.querySelectorAll('.plus-minus-toggle');
 
     faqsButtons.forEach(faqsButton => {
         faqsButton.addEventListener("click", () => {
             faqsButton.classList.toggle('active');
             faqsButton.closest('.faqs-box').querySelector('.faqs-box-answer').classList.toggle('active');
-            
-            if (faqsButton.classList.contains('active')) {
-                faqsButton.querySelector("img").src = "./../images/icons/plus.svg"
-            } else {
-                faqsButton.querySelector("img").src = "./../images/icons/minus.svg"
-            }
-
-    
         }) 
    });
 
