8c050c89a716205a8a0dcd1e102c4d0a0b739364
Dan13lpa <dan1elpa@gmail.com>
2023-07-14 14:06:28 +0200

    last update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ const app = express();
 const multer = require('multer');
 const dotenv = require('dotenv').config()
 const process = require('process')
-
 var corsOptions = {
     origin: [process.env.BASE_URL, 'http://localhost:8081', 'http://127.0.0.1:5500', 'http://127.0.0.1:5501']
 };
