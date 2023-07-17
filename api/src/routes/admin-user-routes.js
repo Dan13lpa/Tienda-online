da00f9f72176b52bd14d146ac342ca18a8e4508e
Dan13lpa <dan1elpa@gmail.com>
2023-07-17 08:50:24 +0200

    token auth

--- api/src/routes/admin-user-routes.js
+++ api/src/routes/admin-user-routes.js
@@ -13,7 +13,7 @@ module.exports = (app, upload) => {
     });
   
     router.post("/",  [authJwt.verifyUserToken], controller.create);
-    router.get("/", [authJwt.verifyUserToken], controller.findAll);  
+    router.get("/", controller.findAll);  
     router.get("/:id", [authJwt.verifyUserToken], controller.findOne);  
     router.put("/:id", [authJwt.verifyUserToken], controller.update);  
     router.delete("/:id", [authJwt.verifyUserToken], controller.delete);
